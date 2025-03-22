c0c6b6eb8adb626e8f5b2d8bca46bb40143eea28
MarioRRom <mariorromyt@gmail.com>
2025-03-22 07:14:42 -0300

    Dots V0.4 | Restyle & Bug Fixs

--- config/bspwm/config/eww/centerpanel/notifications.sh
+++ config/bspwm/config/eww/centerpanel/notifications.sh
@@ -18,8 +18,7 @@ json_dunst=$(dunstctl history)
 
 # Extraer los datos de las notificaciones
 notifications=$(echo "$json_dunst" | jq -r '.data[0][] |
-  "\(.body.data)\n\(.summary.data)\n\(.appname.data)\n\(.icon_path.data)"')
-
+  "\(.body.data)\n\(.summary.data)\n\(.appname.data)\n\(.icon_path.data)"' | sed "s/'/’/g")
 
 # Ahora que tenemos las notificaciones, las formateamos para el widget de Eww
 if [ -z "$notifications" ]; then
@@ -27,38 +26,41 @@ if [ -z "$notifications" ]; then
   echo "
     (box
         :orientation 'v'
-        :space-evenly true
-        :height 492
-        :width 320
-        :spacing -300
-        (label :class 'sin-not-bell' :text '󰂚')
+        :space-evenly false
+        (label :class 'sin-not-bell' :text '󰂚' :style {NOW-PLAYING == '' ? 'margin: 100px -20px -10px -36px' : 'margin: 40px -20px -10px -36px'})
         (label :class 'sin-not-text' :text 'Sin Notificaciones')
     )"
 else
   # Crear un widget para mostrar las notificaciones
   formatted_notifications=""
   
-  # Leemos todas las notificaciones
   # Convertir las notificaciones en un array de líneas para procesarlas
-  # Cada bloque de notificación contiene 4 líneas, que se asignan de manera ordenada
-  while IFS= read -r contenido && IFS= read -r titulo && IFS= read -r appname && IFS= read -r icono; do
-    # Formateamos cada notificación como widget
-    formatted_notifications="$formatted_notifications
-        (notificacion-card :titulo '$titulo' :contenido '$contenido' :imagen '$icono' :app '$appname')"
-  done <<< "$notifications"
+  # Cada bloque de notificación contiene 4 líneas
+  mapfile -t notification_lines <<< "$notifications"
+
+  # Procesar las notificaciones en bloques de 4 líneas
+  for ((i=0; i<${#notification_lines[@]}; i+=4)); do
+      contenido="${notification_lines[i]}"
+      titulo="${notification_lines[i+1]}"
+      appname="${notification_lines[i+2]}"
+      icono="${notification_lines[i+3]}"
+
+
+      # Verificar si los campos están vacíos y asignar valores predeterminados si es necesario
+      [ -z "$appname" ] && appname="Desconocido"
+      [ -z "$icono" ] && icono="/usr/share/icons/Papirus-Dark/48x48/apps/bell.svg"
+
+      # Formateamos cada notificación como widget
+      formatted_notifications="$formatted_notifications
+          (notificacion-card :titulo '$titulo' :contenido '$contenido' :imagen '$icono' :app '$appname')"
+  done
 
   # Mostramos todas las notificaciones en un scrollable box
   echo "
-    (scroll
-        :vscroll true
-        :height 492
-        :width 320
         (box
-            :class 'scrollbox'
             :orientation 'v'
             :spacing 10
             :space-evenly false
             $formatted_notifications
-        )
-    )"
+        )"
 fi
--- config/bspwm/config/eww/centerpanel/now-playing.sh
+++ config/bspwm/config/eww/centerpanel/now-playing.sh
@@ -12,4 +12,24 @@
 #                 https://github.com/MarioRRom/bspwm-dotfiles
 #===========================================================================
 
-paplay ~/.config/bspwm/assets/notification.wav
+
+# Extraer los datos de la canción actual usando playerctl
+artista=$(playerctl metadata artist 2>/dev/null)
+titulo=$(playerctl metadata title 2>/dev/null)
+imagen=$(playerctl metadata mpris:artUrl 2>/dev/null)
+status=$(playerctl status 2>/dev/null)
+color=$(convert "$imagen" -resize 100x100 -colors 1 -format "%c" histogram:info:- | sort -n | head -n 1 | awk '{print $3}')
+
+# Determinar el icono de estado
+if [ "$status" == "Playing" ]; then
+  icono_estado="󰏥"
+else
+  icono_estado=""
+fi
+
+# Verificar si hay una canción reproduciéndose
+if [ -n "$artista" ] && [ -n "$titulo" ]; then
+  echo "(now-playing-card :artista '$artista' :titulo '$titulo' :imagen '$imagen' :status '$icono_estado' :bgcolor '$color')"
+else
+  echo ""
+fi
